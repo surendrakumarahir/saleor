03e76db3e00931de90fa042d86132186e25fc26a
surendra.kumar <surendra.kumar@kinimatic.com>
2026-03-27 13:50:17 +0530

    Added cod payment method

--- saleor/settings.py
+++ saleor/settings.py
@@ -892,6 +892,7 @@ BUILTIN_PLUGINS = [
     "saleor.payment.gateways.braintree.plugin.DeprecatedBraintreeGatewayPlugin",
     "saleor.payment.gateways.razorpay.plugin.DeprecatedRazorpayGatewayPlugin",
     "saleor.payment.gateways.authorize_net.plugin.AuthorizeNetGatewayPlugin",
+    "saleor.payment.gateways.cod.plugin.CODGatewayPlugin",
     "saleor.plugins.user_email.plugin.UserEmailPlugin",
     "saleor.plugins.admin_email.plugin.AdminEmailPlugin",
     "saleor.plugins.sendgrid.plugin.DeprecatedSendgridEmailPlugin",
